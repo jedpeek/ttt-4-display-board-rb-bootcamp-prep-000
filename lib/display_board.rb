7ec66ada9d2c09c4c110b8ddc8fc12de815cc098
Jed Peek <jedpeek@yahoo.com>
2018-05-09 17:51:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" "," "," "," ","X"," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   top_cell = " #{board[0]} | #{board[1]} | #{board[2]} "
   mid_cell = " #{board[3]} | #{board[4]} | #{board[5]} "

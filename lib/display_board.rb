a0ebc115da0df9ed36e2a55e3b8609ebc5fc9ccc
Jed Peek <jedpeek@yahoo.com>
2018-05-09 17:33:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,9 @@
 # out the current state.
 board = [" "," "," "," ","X"," "," "," "," "]
 def display_board(board)
-
+  cell = "   |   |   "
+  row = "\n-----------\n"
+  board = cell + row + cell + row + cell + "\n"
+  print board
+end
 display_board(board)
